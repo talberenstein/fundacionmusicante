04d71ef6d0771bbd2ca2ce7e5acd02a7851ba2be
Tal Zeev <zeevtorrestal@gmail.com>
2017-06-28 11:59:17 -0400

    first commit

---
+++ enviar.php
@@ -0,0 +1,25 @@
+<?php
+$nombre = $_POST['nombre'];
+$mail = $_POST['mail'];
+$empresa = $_POST['empresa'];
+$telefono = $_POST['telefono'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",
+ de la empresa " . $empresa . " \r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Su teléfono es: " . $telefono . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'mcnamarath@hotmail.com';
+$asunto = 'Asunto del mail recibido';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+echo 'Mensaje enviado correctamente';
+?>
